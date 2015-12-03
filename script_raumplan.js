01854951a837b39f68dba3961f1e8d9dcd58e70a
bluvel <bluvel@users.noreply.github.com>
2015-12-03 17:47:52 +0100

    Create script_raumplan.js

---
+++ script_raumplan.js
@@ -0,0 +1,164 @@
+
+var main=function() {
+  var CANVAS=document.getElementById("your_canvas");
+  CANVAS.width=window.innerWidth;
+  CANVAS.height=window.innerHeight;
+
+  /*========================= CAPTURE MOUSE EVENTS ========================= */
+
+  var drag=false;                                                               
+
+  var old_x, old_y;
+
+  var mouseDown=function(e) {
+    drag=true;
+    old_x=e.pageX, old_y=e.pageY;
+    e.preventDefault();
+    return false;
+  };
+
+  var mouseUp=function(e){
+    drag=false;
+  };
+
+  var mouseMove=function(e) {
+    if (!drag) return false;
+    var dX=e.pageX-old_x,
+        dY=e.pageY-old_y;
+    THETA+=dX*2*Math.PI/CANVAS.width;
+    PHI+=dY*2*Math.PI/CANVAS.height;
+    old_x=e.pageX, old_y=e.pageY;
+    e.preventDefault();
+  };
+
+  CANVAS.addEventListener("mousedown", mouseDown, false);
+  CANVAS.addEventListener("mouseup", mouseUp, false);
+  CANVAS.addEventListener("mouseout", mouseUp, false);
+  CANVAS.addEventListener("mousemove", mouseMove, false);
+
+  /*========================= GET WEBGL CONTEXT ========================= */
+  var GL;
+  try {
+    GL = CANVAS.getContext("experimental-webgl", {antialias: true});
+  } catch (e) {
+    alert("You are not webgl compatible :(") ;
+    return false;
+  }
+
+  /*========================= SHADERS ========================= */
+  /*jshint multistr: true */
+
+  var shader_vertex_source="\n\
+attribute vec3 position;\n\
+uniform mat4 Pmatrix;\n\
+uniform mat4 Vmatrix;\n\
+uniform mat4 Mmatrix;\n\
+attribute vec3 color; //the color of the point\n\
+varying vec3 vColor;\n\
+void main(void) { //pre-built function\n\
+gl_Position = Pmatrix*Vmatrix*Mmatrix*vec4(position, 1.);\n\
+vColor=color;\n\
+}";
+
+  var shader_fragment_source="\n\
+precision mediump float;\n\
+varying vec3 vColor;\n\
+void main(void) {\n\
+gl_FragColor = vec4(vColor, 1.);\n\
+}";
+
+  var get_shader=function(source, type, typeString) {
+    var shader = GL.createShader(type);
+    GL.shaderSource(shader, source);
+    GL.compileShader(shader);
+    if (!GL.getShaderParameter(shader, GL.COMPILE_STATUS)) {
+      alert("ERROR IN "+typeString+ " SHADER : " + GL.getShaderInfoLog(shader));
+      return false;
+    }
+    return shader;
+  };
+
+  var shader_vertex=get_shader(shader_vertex_source, GL.VERTEX_SHADER, "VERTEX");
+  var shader_fragment=get_shader(shader_fragment_source, GL.FRAGMENT_SHADER, "FRAGMENT");
+
+  var SHADER_PROGRAM=GL.createProgram();
+  GL.attachShader(SHADER_PROGRAM, shader_vertex);
+  GL.attachShader(SHADER_PROGRAM, shader_fragment);
+
+  GL.linkProgram(SHADER_PROGRAM);
+
+  var _Pmatrix = GL.getUniformLocation(SHADER_PROGRAM, "Pmatrix");
+  var _Vmatrix = GL.getUniformLocation(SHADER_PROGRAM, "Vmatrix");
+  var _Mmatrix = GL.getUniformLocation(SHADER_PROGRAM, "Mmatrix");
+
+  var _color = GL.getAttribLocation(SHADER_PROGRAM, "color");
+  var _position = GL.getAttribLocation(SHADER_PROGRAM, "position");
+
+  GL.enableVertexAttribArray(_color);
+  GL.enableVertexAttribArray(_position);
+
+  GL.useProgram(SHADER_PROGRAM);
+
+  /*========================= THE CUBE ========================= */
+  //POINTS :
+  var cube_vertex=LIBS.cube_coordinates();
+
+  var CUBE_VERTEX= GL.createBuffer ();
+  GL.bindBuffer(GL.ARRAY_BUFFER, CUBE_VERTEX);
+  GL.bufferData(GL.ARRAY_BUFFER,
+                new Float32Array(cube_vertex),
+    GL.STATIC_DRAW);
+
+  //FACES :
+  var cube_faces =LIBS.cube_faces();
+          
+  var CUBE_FACES= GL.createBuffer ();
+  GL.bindBuffer(GL.ELEMENT_ARRAY_BUFFER, CUBE_FACES);
+  GL.bufferData(GL.ELEMENT_ARRAY_BUFFER,
+                new Uint16Array(cube_faces),
+    GL.STATIC_DRAW);
+
+  /*========================= MATRIX ========================= */
+
+  var PROJMATRIX=LIBS.get_projection(40, CANVAS.width/CANVAS.height, 1, 100);
+  var MOVEMATRIX=LIBS.get_I4();
+  var VIEWMATRIX=LIBS.get_I4();
+
+  LIBS.translateZ(VIEWMATRIX, -6);
+  var THETA=0,
+      PHI=0;
+
+  /*========================= DRAWING ========================= */
+  GL.enable(GL.DEPTH_TEST);
+  GL.depthFunc(GL.LEQUAL);
+  GL.clearColor(0.0, 0.0, 0.0, 0.0);
+  GL.clearDepth(1.0);
+
+  var time_old=0;
+  var animate=function(time) {
+    var dt=time-time_old;
+    LIBS.set_I4(MOVEMATRIX);
+    LIBS.rotateY(MOVEMATRIX, THETA);
+    LIBS.rotateX(MOVEMATRIX, PHI);
+    time_old=time;
+
+    GL.viewport(0.0, 0.0, CANVAS.width, CANVAS.height);
+    GL.clear(GL.COLOR_BUFFER_BIT | GL.DEPTH_BUFFER_BIT);
+
+    GL.uniformMatrix4fv(_Pmatrix, false, PROJMATRIX);
+    GL.uniformMatrix4fv(_Vmatrix, false, VIEWMATRIX);
+    GL.uniformMatrix4fv(_Mmatrix, false, MOVEMATRIX);
+
+    GL.bindBuffer(GL.ARRAY_BUFFER, CUBE_VERTEX);
+    GL.vertexAttribPointer(_position, 3, GL.FLOAT, false,4*(3+3),0) ;
+    GL.vertexAttribPointer(_color, 3, GL.FLOAT, false,4*(3+3),3*4) ;
+
+    GL.bindBuffer(GL.ELEMENT_ARRAY_BUFFER, CUBE_FACES);
+    GL.drawElements(GL.TRIANGLES,12*2*3, GL.UNSIGNED_SHORT, 0);
+
+    GL.flush();
+
+    window.requestAnimationFrame(animate);
+  };
+  animate(0);
+};
