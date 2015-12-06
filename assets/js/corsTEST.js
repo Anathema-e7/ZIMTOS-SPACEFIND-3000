285ea26331136db5cad28e82b0078da0665c593d
Nate <anon008@inbox.com>
2015-12-06 11:48:07 +0100

    okay so the call _has_ to be synchronous? weird stuff. js is giving me hell.

--- assets/js/corsTEST.js
+++ assets/js/corsTEST.js
@@ -1,5 +1,6 @@
-var bloecke = [];
-
+$( document ).ready(function() {
+    var bloecke = [];
+ids = [];
 jqhxr = $.ajax({
     type:     "POST",
     url:      'https://cors-anywhere.herokuapp.com/http://www.oszimt.de/stundenplan/KPlan1.php',
@@ -7,6 +8,7 @@ jqhxr = $.ajax({
 data: "Turnus[]=jede+Woche&cbxAlleTurnusse=1&Fach[]=DE31&Fach[]=EN31&Fach[]=IT31&Fach[]=MA31&Fach[]=MI31&Fach[]=ch31&Fach[]=de31&Fach[]=en31&Fach[]=ge31&Fach[]=ku31&Fach[]=ma31&Fach[]=ph31&Fach[]=ph32&Fach[]=pw31&Fach[]=sn31&Fach[]=sp11&Fach[]=sp12&Fach[]=sp13&Fach[]=sp31&Fach[]=sp32&Fach[]=wa&Fach[]=ww31&cbxAlleFaecher=1&Klasse=OG+3&Version=1449069041",
 // data: "cbxAlleTurnusse=1&cbxAlleFaecher=1&Klasse=OG+3&Version=1449069041",
     dataType: "html",
+    async: false,
     success: function(data) {
 	$(data).find( "table.plan td.plan_inhalt > table" ).map(function() {
 	    var obj = {},
@@ -19,29 +21,68 @@ data: "Turnus[]=jede+Woche&cbxAlleTurnusse=1&Fach[]=DE31&Fach[]=EN31&Fach[]=IT31
 		obj["Fach"] = td.eq(0).text();
 		//TODO add wochenlogik // yea maybe not. fu webuntis
 		obj["Raum"]       = td.eq(1).text();
-		obj["ID"]       = wochentag +""+ block;
+		obj["id"]       = wochentag +""+ block +"r"+td.eq(1).text();
+		ids.push(wochentag +""+ block);
 		obj["Lehrer"]     = td.eq(2).text();
 		// obj["_Block"]     = tr.parent().parent().parent().parent().index() - 1;
-		// obj["_Wochentag"] = td.parent().parent().parent().parent().index() - 1;
+		obj["_Block"]     = $(this).closest("td.plan_inhalt").index();
+		obj["_Wochentag"] = td.parent().parent().parent().parent().index() - 1;
 	    }
 	    else if (info.hasClass("kplan_inhalt_bg_vertretung")) {
 		//this could need fixing
 		obj["Fach"]      = td.eq(1).text();
 		obj["Raum"]      = td.eq(2).text();
 		obj["Lehrer"]    = td.eq(3).text();
+		ids.push(wochentag +""+ block);
+		obj["id"]       = wochentag +""+ block +"r"+td.eq(1).text();
 		obj["Vertetung"] = true;
 	    } else if (info.hasClass("kplan_inhalt_bg_ausfall")) {
 		//LEHRER NISCHT GEFUNDEN....
 		// naja trotzdem adden...
 		obj["Fach"]      = td.eq(1).text();
 		obj["Raum"]      = td.eq(2).text();
+		ids.push(wochentag +""+ block);
+		obj["id"]       = wochentag +""+ block +"r"+td.eq(1).text();
 		obj["Lehrer"]    = td.eq(3).text();
 		obj["Vertetung"] = true;
 	    }
 	    bloecke.push(obj);
 	})   
     }});
-console.log(bloecke);
+// console.log(bloecke.sort());
+    console.log(bloecke);
+// console.log(bloecke[10]);
+    console.log(ids);
+// var result = $.grep(bloecke, function(e){ return e.id == "11"; });
+// console.log(result);
+    console.log(JSON.stringify(bloecke[0]));
+// var lookup = {};
+// for (var i = 0, len = bloecke.length; i < len; i++) {
+    // lookup[bloecke[i].id] = bloecke[i];
+// }
 
 
-    
+// function sortByKey(array, key) {
+//     return array.sort(function(a, b) {
+//         var x = a[key]; var y = b[key];
+//         return ((x < y) ? -1 : ((x > y) ? 1 : 0));
+//     });
+// }
+// b = sortByKey(bloecke,"id");
+// console.log(b);
+// // console.log(lookup[11]);
+// // console.log(lookup);
+
+//     var ul = $('<ul>').appendTo('body');
+//     var json = { items: ['item 1', 'item 2', 'item 3'] };
+// // var json = bloecke;
+//     $(json.items).each(function(index, item) {
+//         ul.append($(document.createElement('li')).text(item));
+//     });
+    console.log(bloecke[3]);
+    $.each($(bloecke[0]), function( key, value ) {
+	// alert( index + ": " + value );
+        ul.append($(document.createElement('li')).text(value));
+	
+    });  
+});
