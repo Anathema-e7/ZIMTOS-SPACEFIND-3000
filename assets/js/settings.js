28961aea94245e170568888edad1736366c56f52
Nate <anon008@inbox.com>
2015-12-11 23:22:12 +0100

    settings update

---
+++ assets/js/settings.js
@@ -0,0 +1,36 @@
+
+var store = localStorage;
+$( document ).ready(function() {
+
+function isStorageFilled() {
+    // store.getItem("MA31")==null ? alert("empty") : alert ("full");
+    return    store.getItem("MA31")!=null;
+}
+
+   
+    $("#plan").click(function() {
+	// checkStorage() ? $("#stundenplan").click() : $("#planwahl").click();
+	isStorageFilled() ? window.open("stundenplan.html") : $("#planwahl").click();
+    });
+
+    $("#settingsbtn").click(function() {
+	fillStorageWithCourses();
+	window.open("stundenplan.html");
+    });
+
+    $("#switchall").click(function() {
+	$(":checkbox").click();
+    });
+
+function fillStorageWithCourses() {
+    // alert("yes");
+    $(":checkbox").each(function() {
+	// alert($(this).attr("id"));
+	var id = $(this).attr("id");
+	var val = $(this).prop("checked");
+	store.setItem(id, val);
+	// alert( id + val);
+    });
+}
+
+});
