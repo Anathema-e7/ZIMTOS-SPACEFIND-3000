3d6c19558848317767ee40fb386faa7024224b5e
Nate <anon008@inbox.com>
2015-12-14 20:29:40 +0100

    links fixed, again

--- assets/js/linklogic.js
+++ assets/js/linklogic.js
@@ -1,4 +1,7 @@
 $( document ).ready(function() {
+
+    //go to stundenplan only when storage is populated
+    //could add a message to the user here...
     $("#plan").click(function() {
 	isStorageFilled() ? window.location.href = "stundenplan.html" : window.location.href = "planwahl.html";
     });
