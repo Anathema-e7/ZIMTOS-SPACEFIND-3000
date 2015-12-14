70bef3bbf246f50dadab0d194b835b68978520c0
Nate <anon008@inbox.com>
2015-12-14 20:41:57 +0100

    some comments, cleanup

--- assets/js/stundenplan.js
+++ assets/js/stundenplan.js
@@ -2,9 +2,8 @@ $( document ).ready(function() {
 
     unixdate = "1449671354"
     og3data = "Turnus[]=a&Turnus[]=b&Turnus[]=jede+Woche&cbxAlleTurnusse=1&Fach[]=DE31&Fach[]=EN31&Fach[]=IT31&Fach[]=MA31&Fach[]=MI31&Fach[]=ch31&Fach[]=de31&Fach[]=en31&Fach[]=ge31&Fach[]=ku31&Fach[]=ma31&Fach[]=ph31&Fach[]=ph32&Fach[]=pw31&Fach[]=sn31&Fach[]=sp11&Fach[]=sp12&Fach[]=sp13&Fach[]=sp31&Fach[]=sp32&Fach[]=wa&Fach[]=ww31&cbxAlleFaecher=1&Klasse=OG+3&Version="
-
-    
     arr=[];
+
     function asyncCall() { 
 	$.ajax({
 	    type:     "POST",
@@ -12,17 +11,14 @@ $( document ).ready(function() {
 	    data: og3data+unixdate,
 	    dataType: "html",
 	    crossDomain: true,
-	    // contentType: "charset=ISO-8859-1",
 	    async: false,
 	    success: function(data) {
 		result=data;
-		// alert(result);
 	    },
 	    error: function() {
 		alert("yay everything broke");
 	    }
 	})
-	// if (result==null) return alert("Ermagerd");
 	return result;
     };
 
@@ -30,16 +26,17 @@ $( document ).ready(function() {
     //this will remove any images from the HTML _before_ putting it in a DOM object, so that they won't be requested. less bandwidth.
     data = data.replace(/<img[^>]+>/gi, "");
     
+    //fancy jQuery selectors
     $(data).find( "table.plan td.plan_inhalt > table" ).map(function() {
 	var obj = {},
 	    td = $(this).find('td'),
 	    tr = $(this).find('tr'),
-	    block = tr.parent().parent().parent().parent().index() - 1,
+	    block = tr.parent().parent().parent().parent().index() - 1, //this could be better
 	    wochentag = td.parent().parent().parent().parent().index() - 1,
 	    info = td.parent().parent().parent();
 
 	if (info.hasClass("kplan_inhalt_bg_normal")) {		
-	    obj.fach      = td.eq(0).text().replace(/\s\((a|b)\)/,"");
+	    obj.fach      = td.eq(0).text().replace(/\s\((a|b)\)/,""); //removing wochenlogik
 	    obj.raum   = td.eq(1).text();
 	    obj.id     = wochentag +""+ block;
 	    obj.lehrer = td.eq(2).text().substr(4);
@@ -58,10 +55,12 @@ $( document ).ready(function() {
 	}
 
 	arr.push(obj);
+	//writing storage
 	localStorage.setItem(obj.id+obj.lehrer,JSON.stringify(obj));
     });
     
     function findBlock(idv) {
+	//linux intensifies
 	var result = $.grep(arr, function(e){ return (e.id == idv); });
 	if (result.length == 0) {
 	    return [null];
@@ -76,17 +75,20 @@ $( document ).ready(function() {
 	if (t[0] !=null) {
 	    for(var i=0; i<t.length; i++) {
 		tc=t[i];
+		//leaving this for historical reasons
 		// if (typeof tc === "undefined") {
 		// } else { 
 		if(localStorage.getItem(tc.fach)=="true") {
 		    $("#b"+idv).show();
 
+		    //colorize block
 		    if(tc.ausfall==true) {
 			$("#b"+idv + " > div").attr("class","panel panel-warning");
 		    } else if(tc.vertretung==true) {
 			$("#b"+idv + " > div").attr("class","panel panel-info");
 		    }
-		    
+
+		    //set block text
 		    document.getElementById('k'+idv).innerHTML = tc.fach;
 		    document.getElementById('r'+idv).innerHTML = tc.raum;
 		    document.getElementById('l'+idv).innerHTML = tc.lehrer;
