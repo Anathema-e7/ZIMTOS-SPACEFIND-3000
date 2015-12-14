1223b8960bcca05e0e6d935fe136bbe0b7a8103b
Nate <anon008@inbox.com>
2015-12-14 11:56:23 +0100

    added lehrersuche functionality

--- assets/js/search.js
+++ assets/js/search.js
@@ -3,16 +3,28 @@ $( document ).ready(function() {
 // $(".dropdown-menu li a").click(function(){
 $("#go").click(function(){
 
-    alert($("#dropdownMenu2").text());
+    // alert($("#dropdownMenu2").text().substr(0,1));
+    w = $("#dropdownMenu2").val();
+    b = $("#dropdownMenu1").val();
+    l = $("#lname").val();
+    s = w+b+l;
+    f = JSON.parse(localStorage.getItem(s));
+    if (f!=null) {
+    $("#outputLehrer").text(f.raum);
+    } else { 
+	$("#outputLehrer").text("Lehrer nicht gefunden");
+    }
 });
 
 $("#b li a").click(function(){
-
       $("#dropdownMenu2:first-child").text($(this).text());
-      // $(".btn:first-child").val($(this).text());
-      // $(".btn:first-child").text($(this).text());
-      // $(".btn:first-child").val($(this).text());
+      $("#dropdownMenu2:first-child").val($(this).attr("id"));
 
    });
 
+$("#w li a").click(function(){
+      $("#dropdownMenu1:first-child").text($(this).text());
+      $("#dropdownMenu1:first-child").val($(this).attr("id"));
+
+});
 });
